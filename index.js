5c7b5205e7958612ae637460c6e29d5067b933b2
taha zayed <tahazayed@gmail.com>
2017-06-09 00:52:53 +0200

    2017-06-09

--- index.js
+++ index.js
@@ -102,6 +102,9 @@ function sendTextMessage(sender, text) {
 }
 
 function sendGenericMessage(sender, text) {
+	var dateFormat = require('dateformat');
+
+
 	let messageData = {
 		"attachment": {
 			"type": "template",
@@ -109,7 +112,7 @@ function sendGenericMessage(sender, text) {
 				"template_type": "generic",
 				"elements": [{
 					"title": text.title,
-					"subtitle": text.date,
+					"subtitle": dateFormat(new Date(text.date), "dddd, mmmm dS, yyyy");,
 					"image_url": "https://egypttravelwarningbot.herokuapp.com/static/img/red-warning-sign2.jpg",
 					"buttons": [{
 						"type": "web_url",
